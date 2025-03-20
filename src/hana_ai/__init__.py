c6e8c39f606a3d6c1f635b823453f5d7eff51b2d
Yao <raymond.yao@sap.com>
2025-03-20 16:33:26 +0800

    udpate version display

--- src/hana_ai/__init__.py
+++ src/hana_ai/__init__.py
@@ -2,6 +2,4 @@
 hana_ai is a Python package for AI/ML related utilities.
 """
 
-# load version from version.txt to __version__
-with open('version.txt') as f:
-    __version__ = f.read().strip()
+__version__ = "1.0.25031900"
