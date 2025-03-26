36773e7f44839cedb0b8e360095283022f7a0581
Yao <raymond.yao@sap.com>
2025-03-26 11:11:05 +0800

    update release.yaml

--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
 from setuptools import setup, find_packages
 
-def get_version():
-    """ fancy doc to satisfy linter """
-    with open('version.txt') as ver_file:
-        version_str = ver_file.readline().rstrip()
-    return version_str
-
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
+install_requires = [
+        'langchain',
+        'numpy',
+        'pandas',
+        'hana-ml>=2.24.25031800',
+        'langchain-community',
+        'langchain-core',
+        'langchain-experimental',
+        'langchain-text-splitters',
+        'pydantic',
+        'pydantic-core',
+        'generative-ai-hub-sdk'
+]
 
 setup(
     name='hana_ai',
-    version=get_version(),
+    version="1.0.25031900",
     author='SAP',
     license='Apache License 2.0',
     packages=find_packages(where='src'),
@@ -19,7 +24,7 @@ setup(
     package_data={'hana_ai': ['vectorstore/knowledge_base/sql_knowledge/*',
                               'vectorstore/knowledge_base/python_knowledge/*',
                               'include src/hana_ai/agents/scenario_knowledge_base/*']},
-    install_requires=required,
     include_package_data=True,
+    install_requires=install_requires,
     python_requires='>=3.0'
 )
