02b96b3e1963bfa0654fbee1a17a484509932544
Yao <raymond.yao@sap.com>
2025-04-03 15:50:56 +0800

    update fixed version of hana-ml

--- src/hana_ai/__init__.py
+++ src/hana_ai/__init__.py
@@ -1,5 +1,14 @@
+import os
+import hana_ai
+
+
 """
 hana_ai is a Python package for AI/ML related utilities.
 """
 
-__version__ = "1.0.25032800"
+# The root directory of the package
+root_dir = os.path.dirname(hana_ai.__file__)
+
+version_file = os.path.join(root_dir, "..", "..", "version.txt")
+with open(version_file, "r") as f:
+    __version__ = f.read().strip()
